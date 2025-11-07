2ec8b44d9aabdbab37bbe15498c5a87c27b1283e
catarena2608 <thodeptrai11111@gmail.com>
2025-11-07 20:57:45 +0700

    first commit

--- BackEnd_auth/server.js
+++ BackEnd_auth/server.js
@@ -22,6 +22,6 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ Auth service connected to MongoDB"))
   .catch(err => console.error("❌ MongoDB error:", err));
 
-app.listen(process.env.PORT || 4001, () =>
+app.listen(process.env.PORT || 3001, () =>
       console.log(`🚀 Post service running on port ${process.env.PORT || 3001}`)
     );
